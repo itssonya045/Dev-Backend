032042937837d02b141f301da6b3ad1bb2dabd02
itssonya045 <shrirakshe2004@email.com>
2025-08-24 16:28:56 +0530

    connected to the database

--- src/app.js
+++ src/app.js
@@ -1,23 +1,15 @@
 const express = require("express");
-const { addmainAuth,userAuth } = require("./middleware/auth"); // or default export
-// const addmainAuth = require("./middleware/auth"); // if using default export
+const connectDB = require("./config/database")
 
-const app = express();   // ✅ define app first
+const app = express();   
 
-// use middleware AFTER defining app
-app.use("/admin", addmainAuth);
+connectDB().then(()=>{
+    console.log("successfully connected database.")
 
-app.get("/admin/getdata", (req, res) => {
-  res.send("All data is sent");
-});
-
-app.get("/admin/deletedata", (req, res) => {
-  res.send("Data is deleted");
-});
-
-app.get("/user",userAuth,(req,res)=>{
-    res.send("user data send")
-})
+}).catch(()=>{
+    console.log("unsuccessful connection")
+}
+)
 
 app.listen(7777, () => {
   console.log("Server working well on port 7777");
