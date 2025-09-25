7612dbe79aa26f2dae154f2da82de1152b434fa0
itssonya045 <shrirakshe2004@email.com>
2025-09-25 14:29:59 +0530

    Validation For UserID - ID Present DB Or Not

--- src/routes/requestRouter.js
+++ src/routes/requestRouter.js
@@ -2,6 +2,9 @@ const express = require("express");
 const requestRouter = express.Router();
 const { userAuth } = require("../middleware/auth");
 const ConnectionRequest = require("../models/connectionreqmodel"); // ✅ default import
+const User = require("../models/user")
+const mongoose = require("mongoose");
+
 
 requestRouter.post("/request/send/:status/:toUserId", userAuth, async (req, res) => {
   try {
@@ -14,6 +17,29 @@ requestRouter.post("/request/send/:status/:toUserId", userAuth, async (req, res)
       return res.status(400).json({ message: "Invalid status: " + status });
 
     }
+
+
+    
+
+// First check if toUserId is a valid MongoDB ObjectId
+if (!mongoose.Types.ObjectId.isValid(toUserId)) {
+  return res.status(400).json({ message: "Invalid User ID" });
+}
+
+// Find the user by ID
+const toUser = await User.findById(toUserId);
+
+// If user not found, return 404
+if (!toUser) {
+  return res.status(404).json({ message: "User ID does NOT exist in DB" });
+}
+
+
+
+
+
+  
+
 
     const existingConnectionRequest = await ConnectionRequest.findOne({
       $or:[
